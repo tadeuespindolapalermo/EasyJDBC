78ad479c19014026f6808075577a3ba156ffdb4e
Tadeu Espíndola Palermo <tepalermo@stefanini.com>
2020-02-05 21:55:13 -0300

    Javadoc documentation concerning the @Identifier and @NotColumn annotations.

--- src/main/java/com/github/tadeuespindolapalermo/easyjdbc/annotation/Identifier.java
+++ src/main/java/com/github/tadeuespindolapalermo/easyjdbc/annotation/Identifier.java
@@ -6,6 +6,33 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation must be used to annotate the <b>Getter method</b> of the 
+ * attribute concerning the entity's unique identifier (id). General rules:
+ * 
+ * <ul><li>
+ * If no Getter method is noted, the entity's unique identifier will not 
+ * be recognized, which may cause an error at runtime according to some 
+ * implementation scenarios.
+ * </li><li>
+ * If the entity's unique identifier is self-incrementing, the autoIncrement 
+ * property must be declared with true value, because by default the value 
+ * is implicitly declared as false.
+ * </li></ul>
+ * 
+ * E-mail to report bugs and problems: <i>tadeupalermoti@gmail.com</i>
+ * 
+ * <p>This annotation is part of the library
+ * <a href="https://github.com/tadeuespindolapalermo/EasyJDBC">
+ * Easy JDBC Library</a>
+ *
+ * @author Tadeu Espíndola Palermo 
+ * @since 1.0
+ * 
+ * @see NotColumn
+ * @see PersistentClass
+ * @see PersistentClassNamed   
+ */
 @Retention(RUNTIME)
 @Target(METHOD)
 public @interface Identifier {	 
--- src/main/java/com/github/tadeuespindolapalermo/easyjdbc/annotation/NotColumn.java
+++ src/main/java/com/github/tadeuespindolapalermo/easyjdbc/annotation/NotColumn.java
@@ -6,6 +6,32 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation is useful for specifying that certain attributes of the 
+ * entity do not refer to any column in the database table. General observations:
+ * 
+ * <ul><li>
+ * If this is the scenario, the attribute must be annotated to avoid a 
+ * runtime error, otherwise the attribute will be seen as a column in the 
+ * table concerning the referring entity.
+ * </li><li>
+ * Some entity attributes can only function as an aid or support for 
+ * certain purposes. In such cases, it is essential that they be noted!
+ * </li></ul>
+ * 
+ * E-mail to report bugs and problems: <i>tadeupalermoti@gmail.com</i>
+ * 
+ * <p>This annotation is part of the library
+ * <a href="https://github.com/tadeuespindolapalermo/EasyJDBC">
+ * Easy JDBC Library</a>
+ *
+ * @author Tadeu Espíndola Palermo 
+ * @since 1.0
+ * 
+ * @see Identifier
+ * @see PersistentClass
+ * @see PersistentClassNamed   
+ */
 @Retention(RUNTIME)
 @Target(FIELD)
 public @interface NotColumn {	   
